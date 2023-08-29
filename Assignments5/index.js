fe695eb103a02ded2d6e27be651c8037a9aa2008
HimuHimu112002 <mdhmaktaruzzaman9101@gmail.com>
2023-08-29 22:19:13 +0600

    assignment

--- Assignments5/index.js
+++ Assignments5/index.js
@@ -4,27 +4,6 @@ const http = require('http')
 // =============== requirements-> 1 HTTP Server create=============== 
 let server = http.createServer(function(req, res){
 
-    // =============== requirements-> 7 url “/file-write” create file demo.txt =============== 
-    // =============== requirements-> 8 url server response using res.end() =============== 
-
-    
-    // if(req.url = "/file-write"){
-    //     fs.writeFile('demo.txt','Hello World', function(error){
-    //         if(error){
-
-    //             res.writeHead(200, {'Content-Type': 'text/html'});
-    //             res.write("File write fail");
-    //             res.end();
-    //         }else{
-    //             res.writeHead(200, {'Content-Type': 'text/html'});
-    //             res.write("File write success");
-    //             res.end();
-    //         }
-            
-    //     });
-
-    // }
-
     // =============== requirements-> 4 url “/” then the response home page =============== 
     // =============== requirements-> 5 url “/about” then the response about page =============== 
     // =============== requirements-> 6 url “/contact” then the response contact page =============== 
@@ -37,6 +16,28 @@ let server = http.createServer(function(req, res){
     }else if(req.url == "/about"){
         let data = fs.readFileSync('About.html', 'utf-8')
         res.end(data)
+    }
+
+
+     // =============== requirements-> 7 url “/file-write” create file demo.txt =============== 
+    // =============== requirements-> 8 url server response using res.end() =============== 
+
+    
+    if(req.url = "/file-write"){
+        fs.writeFile('demo.txt','Hello World', function(error){
+            if(error){
+
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write("File write fail");
+                res.end();
+            }else{
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write("File write success");
+                res.end();
+            }
+            
+        });
+
     }
 
 
