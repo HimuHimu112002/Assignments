35c3f35d2bb57d6a732fe9f9a5079eadd5dab7a7
Your Name <mdhmaktaruzzaman9101@gmail.com>
2023-10-02 11:07:06 +0600

    assignments

--- Assignments9/app.js
+++ Assignments9/app.js
@@ -1,6 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const routerApi = require("./src/routers/api")
+const app = new express()
+
+// Sequrity Middleware inport
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
+const cors = require('cors') 
+
+
+// Sequrity Middleware implements
+app.use(cors())
+app.use(helmet())
+app.use(mongoSanitize())
+app.use(xss())
+app.use(hpp())
+
+// rate limiting
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+});
 
 router.use("/api/v1",routerApi)
 
